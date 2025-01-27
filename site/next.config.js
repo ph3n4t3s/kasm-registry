48797843cd403f7770ad25ec7697afac45abf791
ph3n4t3s <105867089+ph3n4t3s@users.noreply.github.com>
2025-01-27 13:54:13 +0100

    Update next.config.js

--- site/next.config.js
+++ site/next.config.js
@@ -2,11 +2,11 @@
 
 const nextConfig = {
   env: {
-    name: 'Kasm Technologies',
-    description: 'The official store for Kasm supported workspaces.',
-    icon: '/img/logo.svg',
-    listUrl: 'https://registry.kasmweb.com/',
-    contactUrl: 'https://kasmweb.com/support',
+    name: 'eTIC-Concept Kasm Registry',
+    description: 'eTIC-Concept store based on Kasm supported workspaces.',
+    icon: 'https://ph3n4t3s.github.io/kasm-registry/1.0/logo.png ',
+    listUrl: 'https://ph3n4t3s.github.io/kasm-registry/',
+    contactUrl: 'https://ph3n4t3s.github.io/kasm-registry/issues',
   },
   reactStrictMode: true,
   swcMinify: true,
